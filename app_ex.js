21b12f3db9c02f2b474945dd41b50479ae4484cc
ominivac <robertokbs@gmail.com>
2024-11-05 23:13:44 -0300

    1 cmmit config proj

---
+++ app_ex.js
@@ -0,0 +1,7 @@
+const {obj,func} = require('./utils')
+console.log(obj)
+console.log(func(3,4))
+console.log(__dirname, __filename)
+
+console.log("Starting app.js");
+
---
+++ utils.js
@@ -0,0 +1,12 @@
+const obj={
+    name: 'John',
+    age: 30,
+    city: 'New York'
+}
+
+const func =  (a,b) => a+b
+
+module.exports = {
+    obj,
+    func
+}
