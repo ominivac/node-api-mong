ee1238de23db0796bc123b32d2311ac3cd97f2ff
ominivac <robertokbs@gmail.com>
2024-11-07 00:07:59 -0300

    primeiro redirect

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-fs.writeFileSync('app.txt', 'hi welcome');
 
-fs.appendFileSync('app.txt', 'roberto sousa');
+app.use('/add-product', (req, res, next) => {
+    console.log('first middleware...');
+    res.send('<form action="product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+})
+
+app.use('/product', (req, res, next) => {
+    res.redirect('/');
+})
+
+
+app.use('/', (req, res, next) => {
+    console.log('second middleware...');
+    res.send('<h1>Hello from express</h1>');
+})
+
+
+app.listen(3000);
--- app_ex.js
+++
@@ -1,7 +0,0 @@
-const {obj,func} = require('./utils')
-console.log(obj)
-console.log(func(3,4))
-console.log(__dirname, __filename)
-
-console.log("Starting app.js");
-
